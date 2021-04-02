ffd389ed1971f53d5c7b1ac189f03573086e631d
Hano555 <hanogy@gmail.com>
2021-04-02 15:16:20 +1000

    deploy my React Breaking Bad Api app

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import CharacterItem from './CharacterItem'
+import Spinner from '../ui/Spinner'
+
+const CharacterGrid = ({isLoading, characters}) => {
+    return isLoading ? <Spinner/> : <section className='cards'>
+        {characters.map(character => (
+            <CharacterItem key={character.char_id} character={character}></CharacterItem>
+        ))}
+    </section>
+}
+
+export default CharacterGrid
